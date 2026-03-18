69e6168899627ee377e357bf1a9158cd9c797973
Dzonex <89349082+nnikola2006@users.noreply.github.com>
2026-03-18 15:34:09 +0100

    Zab dodati header

--- int_u_string.c
+++ int_u_string.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-
-char* formiraj(int n, ...);
-
-int main()
-{
-    char* rez;
-    rez = formiraj(3, 123, 456, 789);
-
-    printf("%s\n", rez);
-
-    free(rez);
-
-    return 0;
-}   
-
-char* formiraj(int n, ...)
-{
-    va_list pok_arg;
-    char* rezultat = calloc(1, sizeof(char));
-    char temp[50];
-
-    va_start(pok_arg, n);
-    int broj; // broj koji se grabi iz arg 
-    for(int i = 0; i < n; i++)
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdarg.h>
+    #include <stdlib.h>
+    
+    char* formiraj(int n, ...);
+    
+    int main()
     {
-        broj = va_arg(pok_arg, int);
-
-        itoa(broj, temp, 10); // baza 10 (decimalni broj)
-
-        rezultat = realloc(rezultat, strlen(rezultat) + strlen(temp) + 1);
-        strcat(rezultat, temp);
+        char* rez;
+        rez = formiraj(3, 123, 456, 789);
+    
+        printf("%s\n", rez);
+    
+        free(rez);
+    
+        return 0;
+    }   
+    
+    char* formiraj(int n, ...)
+    {
+        va_list pok_arg;
+        char* rezultat = calloc(1, sizeof(char));    
+        char temp[50];
+    
+        va_start(pok_arg, n);
+        int broj; // broj koji se grabi iz arg 
+        for(int i = 0; i < n; i++)
+        {
+            broj = va_arg(pok_arg, int);
+    
+            itoa(broj, temp, 10); // baza 10 (decimalni broj)
+    
+            rezultat = realloc(rezultat, strlen(rezultat) + strlen(temp) + 1);
+            strcat(rezultat, temp);
+        }
+        va_end(pok_arg);
+    
+        return rezultat;
     }
-    va_end(pok_arg);
-
-    return rezultat;
-}
